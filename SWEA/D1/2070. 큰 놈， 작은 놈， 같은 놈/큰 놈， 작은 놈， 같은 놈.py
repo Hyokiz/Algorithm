e2f056f37e88858caeb091436b342b6280a77d37
HyeonHyuk Jang <109258146+Hyokiz@users.noreply.github.com>
2022-08-09 23:31:29 +0900

    [D1] Title: 큰 놈, 작은 놈, 같은 놈, Time: 133 ms, Memory: 56,940 KB -BaekjoonHub

---
+++ SWEA/D1/2070. 큰 놈， 작은 놈， 같은 놈/큰 놈， 작은 놈， 같은 놈.py
@@ -0,0 +1,10 @@
+t = int(input())
+
+for tc in range(1, t+1):
+    a, b = map(int, input().split())
+    if a < b:
+        print(f'#{tc} <')
+    elif a > b:
+        print(f'#{tc} >')
+    else:
+        print(f'#{tc} =')
