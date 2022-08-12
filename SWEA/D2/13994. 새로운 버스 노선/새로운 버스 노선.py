2262d3e455e77c6acc30c63ab9b87878e08de802
HyeonHyuk Jang <109258146+Hyokiz@users.noreply.github.com>
2022-08-12 16:25:45 +0900

    [D2] Title: 새로운 버스 노선, Time: 199 ms, Memory: 61,436 KB -BaekjoonHub

--- SWEA/D2/13994. 새로운 버스 노선/새로운 버스 노선.py
+++ SWEA/D2/13994. 새로운 버스 노선/새로운 버스 노선.py
@@ -1,24 +1,24 @@
 for tc in range(1, int(input())+1):
     no_sun = int(input())
-    arr = [0] * 1001
+    arr = [0] * 1001 # 빈 배열 생성
     for _ in range(no_sun):
 
         x, a, b = map(int, input().split())
 
-        if x == 1:
+        if x == 1: # 일반버스일때
             for i in range(a, b + 1):
-                arr[i] += 1
+                arr[i] += 1 # 모든 정류장을 한번씩
 
-        elif x == 2:
+        elif x == 2: # 급행버스일때
             for i in range(a, b + 1, 2):
-                arr[i] += 1
+                arr[i] += 1 # a가 홀수이든, 짝수이든 간격을 2로 해주면 다 들어간다..
 
-        else:
-            for i in range(a, b + 1):
-                if a % 2 == 1 and (i % 3 == 0 and i % 10 != 0):
+        else: # 광역 급행버스일 때
+            for i in range(a, b + 1): 
+                if a % 2 == 1 and (i % 3 == 0 and i % 10 != 0): # 홀수인 경우//()를 쳐줘야 한다.
                     arr[i] += 1
 
-                elif a % 2 == 0 and i % 4 == 0:
+                elif a % 2 == 0 and i % 4 == 0: # 짝수인 경우
                     arr[i] += 1
 
 
