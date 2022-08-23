924cdaf5ed0a864efec8fcfbe1562c165aaeecde
HyeonHyuk Jang <109258146+Hyokiz@users.noreply.github.com>
2022-08-23 12:08:06 +0900

    [D4] Title: [S/W 문제해결 기본] 6일차 - 계산기3, Time: 193 ms, Memory: 57,196 KB -BaekjoonHub

---
+++ SWEA/D4/1224. ［S／W 문제해결 기본］ 6일차 － 계산기3/［S／W 문제해결 기본］ 6일차 － 계산기3.py
@@ -0,0 +1,50 @@
+def infix_to_postfix(word):
+    expression = ''
+    stack = []
+    for token in word:
+        if token in '*/+-()':
+            if not stack or token == '(':
+                stack.append(token)
+            elif token in '*/':
+                while stack and stack[-1] in '*/':
+                    expression += stack.pop()
+                stack.append(token)
+            elif token in '+-':
+                while stack and stack[-1] != '(':
+                    expression += stack.pop()
+                stack.append(token)
+            elif token == ')':
+                while stack and stack[-1] != '(':
+                    expression += stack.pop()
+                stack.pop()
+        else:
+            expression += token
+
+    while stack:
+        expression += stack.pop()
+
+    return expression
+
+def calculate(expression):
+    stack = []
+
+    for token in expression:
+        if token not in '*/+-':
+            stack.append(int(token))
+        else:
+            x = stack.pop()
+            y = stack.pop()
+            if token == '+':
+                stack.append(y + x)
+            elif token == '-':
+                stack.append(y - x)
+            elif token == '*':
+                stack.append(y * x)
+            elif token == '/':
+                stack.append(y / x)
+    return stack[-1]
+
+for t in range(1, 11):
+    n = input()
+    postfix_expression = infix_to_postfix(input())
+    print(f'#{t} {calculate(postfix_expression)}')
