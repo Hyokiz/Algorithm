de9dd621510a7a29857e5d957d25bf4265ec6293
HyeonHyuk Jang <109258146+Hyokiz@users.noreply.github.com>
2022-08-24 17:52:36 +0900

    [D3] Title: [S/W 문제해결 기본] 7일차 - 암호생성기, Time: 151 ms, Memory: 60,628 KB -BaekjoonHub

--- SWEA/D3/1225. ［S／W 문제해결 기본］ 7일차 － 암호생성기/［S／W 문제해결 기본］ 7일차 － 암호생성기.py
+++ SWEA/D3/1225. ［S／W 문제해결 기본］ 7일차 － 암호생성기/［S／W 문제해결 기본］ 7일차 － 암호생성기.py
@@ -7,7 +7,7 @@ for _ in range(10):
         # 1~5까지 감소
         for i in range(1, 6):
             new_q = q[0] - i
-            q = q[1:]
+            q.pop(0)
             q.append(new_q)
             # 마지막 숫자가 0이하일때 0으로 만들어주고 탈출
             if new_q <= 0:
