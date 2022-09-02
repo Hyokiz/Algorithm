70e7d1e3e3c6d018602eaf96fe4c5e1da75df26c
HyeonHyuk Jang <109258146+Hyokiz@users.noreply.github.com>
2022-09-02 15:36:32 +0900

    [Silver V] Title: 소수 찾기, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/1978. 소수 찾기/소수 찾기.py
+++ 백준/Silver/1978. 소수 찾기/소수 찾기.py
@@ -4,7 +4,7 @@ numbers = list(map(int, input().split()))
 cnt = 0
 for i in range(n):
     if numbers[i] > 1:
-        for j in range(2, numbers[i]):
+        for j in range(2, numbers[i]//2+1):
             if numbers[i] % j == 0:
                 break
         else:
