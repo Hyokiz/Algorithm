bfe6f909f8ece35f44e2565f6a26619e3f031264
HyeonHyuk Jang <109258146+Hyokiz@users.noreply.github.com>
2022-09-02 15:36:05 +0900

    [Silver V] Title: 소수 찾기, Time: 72 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Silver/1978. 소수 찾기/소수 찾기.py
@@ -0,0 +1,13 @@
+n = int(input())
+numbers = list(map(int, input().split()))
+
+cnt = 0
+for i in range(n):
+    if numbers[i] > 1:
+        for j in range(2, numbers[i]):
+            if numbers[i] % j == 0:
+                break
+        else:
+            cnt += 1
+
+print(cnt)
