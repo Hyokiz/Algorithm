1f297236cccbe3faa390b2c500094fe01a04f96b
HyeonHyuk Jang <109258146+Hyokiz@users.noreply.github.com>
2022-09-02 16:41:13 +0900

    [Silver V] Title: 소수, Time: 332 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/2581. 소수/소수.py
+++ 백준/Silver/2581. 소수/소수.py
@@ -5,7 +5,7 @@ result = 0
 sig = True
 for i in range(m, n+1):
     if i != 1:
-        for j in range(2, i):
+        for j in range(2, i//2+1):
             if i % j == 0:
                 break
         else:
