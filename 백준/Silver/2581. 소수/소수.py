508e33c8a93135ccd1ab7deacb15f3f9759fe721
HyeonHyuk Jang <109258146+Hyokiz@users.noreply.github.com>
2022-09-02 16:36:14 +0900

    [Silver V] Title: 소수, Time: 604 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/2581. 소수/소수.py
+++ 백준/Silver/2581. 소수/소수.py
@@ -1,18 +1,21 @@
 m = int(input())
 n = int(input())
 
-result = set()
-
+result = 0
+sig = True
 for i in range(m, n+1):
     if i != 1:
         for j in range(2, i):
             if i % j == 0:
                 break
         else:
-            result.add(i)
-
+            result += i
+            if sig:
+                result_min = i
+                sig = False
 if not result:
     print(-1)
 else:
-    print(sum(result))
-    print(min(result))
+    print(result)
+    print(result_min)
+
