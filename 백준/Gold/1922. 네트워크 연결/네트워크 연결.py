f1c79a1aadeeeebca9fb439e3aaf1828c6711c67
HyeonHyuk Jang <109258146+Hyokiz@users.noreply.github.com>
2022-09-29 11:47:35 +0900

    [Gold IV] Title: 네트워크 연결, Time: 428 ms, Memory: 129324 KB -BaekjoonHub

--- 백준/Gold/1922. 네트워크 연결/네트워크 연결.py
+++ 백준/Gold/1922. 네트워크 연결/네트워크 연결.py
@@ -1,39 +1,34 @@
+import sys
+
 def find_set(node):
     if node != parent[node]:
-        parent[node] = find_set(parent[node])
+        parent[node] = find_set(parent[node])   # 경로 압축
     return parent[node]
 
-n = int(input())
-m = int(input())
-edges = []
+
+n = int(sys.stdin.readline())    # 컴퓨터 수
+m = int(sys.stdin.readline())    # 연결의 수
+network = []
+
 for _ in range(m):
-    s, e, w = map(int, input().split())
-    edges.append((w, s, e))
+    s, e, w = map(int, sys.stdin.readline().split()) # 시작, 도착 ,비용
+    network.append((w, s, e))
 
-edges.sort()
-
+network.sort()  # 비용 기준 정렬(오름차순)
 parent = list(range(n + 1))
-counts = 0
-costs = 0
+counts = 0  # 더 이상 간선을 선택하지 않아도 되는 때 판별
+cost = 0    # 총 비용(최소가 되는 값 찾기)
 
-for dist, x, y in edges:
+for w, x, y in network:
     x_root, y_root = find_set(x), find_set(y)
 
+    # Union
     if x_root != y_root:
-        if x_root < y_root:
-            parent[y_root] = x_root
-            costs += dist
-            counts += 1
+        parent[min(x_root, y_root)] = max(x_root, y_root)
+        counts += 1
+        cost += w
 
-            if counts >= n - 1:
-                break
-        
-        else:
-            parent[x_root] = y_root
-            costs += dist
-            counts += 1
-            
-            if counts >= n - 1:
-                break
+        if counts >= n - 1:
+            break
 
-print(costs)
+print(cost)
