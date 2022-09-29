7b087d3b56d014af3daa4d2f100b4cbc4a554611
HyeonHyuk Jang <109258146+Hyokiz@users.noreply.github.com>
2022-09-29 17:14:20 +0900

    [Gold III] Title: 파티, Time: 212 ms, Memory: 116672 KB -BaekjoonHub

--- 백준/Gold/1238. 파티/파티.py
+++ 백준/Gold/1238. 파티/파티.py
@@ -1,15 +1,22 @@
 from heapq import heappush, heappop
 
+# 가는길, 오는길 따로 일방통행인 다익스트라 문제
+# 인자로 거리, 그래프, 시작점을 넣는다.
 def dijkstra(distance, graph, start):
+    # 시작점 거리 초기화
     distance[start] = 0
+    # 거리, 시작점을 heap 에 추가
     heap = [(0, start)]
 
     while heap:
+        # 1. 최단 거리가 가장 짧은 정점 선택
         min_dist, min_node = heappop(heap)
 
+        # 2. 이미 최단 거리로 기록되어 있는 값보다 높은 경우 무시
         if min_dist > distance[min_node]:
             continue
 
+        # 3. 해당 정점에 인접한 정점들에 대해 최단 거리 갱신
         for next_dist, next_node in graph[min_node]:
             new_dist = min_dist + next_dist
             if new_dist < distance[next_node]:
@@ -18,17 +25,18 @@ def dijkstra(distance, graph, start):
 
 
 n, m, x = map(int, input().split())
-graph1 = [[] for _ in range(n + 1)]
-graph2 = [[] for _ in range(n + 1)]
+graph1 = [[] for _ in range(n + 1)]     # 가는길 인접정점
+graph2 = [[] for _ in range(n + 1)]     # 오는길 인접정점
 INF = 99999999
-distance1 = [INF] * (n + 1)
-distance2 = [INF] * (n + 1)
+distance1 = [INF] * (n + 1)     # 가는길 거리
+distance2 = [INF] * (n + 1)     # 오는길 거리
 
 for _ in range(m):
     s, e, w = map(int, input().split())
     graph1[s].append((w, e))
     graph2[e].append((w, s))
 
+# 가는길, 오는길 distance에 추가
 dijkstra(distance1, graph1, x)
 dijkstra(distance2, graph2, x)
 
