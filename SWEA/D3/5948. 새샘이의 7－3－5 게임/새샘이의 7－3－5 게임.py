8721e9febb62e83755caef214b785380ba9bf622
HyeonHyuk Jang <109258146+Hyokiz@users.noreply.github.com>
2022-10-04 22:16:01 +0900

    [D3] Title: 새샘이의 7-3-5 게임, Time: 1,330 ms, Memory: 64,728 KB -BaekjoonHub

--- SWEA/D3/5948. 새샘이의 7－3－5 게임/새샘이의 7－3－5 게임.py
+++ SWEA/D3/5948. 새샘이의 7－3－5 게임/새샘이의 7－3－5 게임.py
@@ -1,20 +1,14 @@
-def combinations(arr, start):
-    if len(arr) == 3:
-        if sum(arr) not in result:
-            result.append(sum(arr))
-            return
-
-    for i in range(start, len(numbers)):
-        arr.append(numbers[i])
-
-        combinations(arr, i + 1)
-
-        arr.pop()
-
+from itertools import combinations
 
 for t in range(1, int(input()) + 1):
     numbers = list(map(int, input().split()))
+    comb = list(combinations(numbers, 3))
     result = []
-    combinations([], 0)
+    for i in comb:
+        if sum(i) not in result:
+            result.append(sum(i))
+
     result.sort(reverse=True)
+
     print(f'#{t}', result[4])
+
