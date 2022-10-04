e4e29c4b18ffcff63ff9620a38426d5f2904c8d2
HyeonHyuk Jang <109258146+Hyokiz@users.noreply.github.com>
2022-10-04 22:45:52 +0900

    [D3] Title: 오목 판정, Time: 192 ms, Memory: 61,680 KB -BaekjoonHub

--- SWEA/D3/11315. 오목 판정/오목 판정.py
+++ SWEA/D3/11315. 오목 판정/오목 판정.py
@@ -1,31 +1,30 @@
-# 하 우 좌하 우하
+# 11315. 오목 판정
+
+# 하, 우, 좌하, 우하
 dx = [1, 0, 1, 1]
 dy = [0, 1, -1, 1]
 
-def check(omok):
-    # 탐색
+def check(x, y):
     for x in range(n):
         for y in range(n):
+            if arr[x][y] == 'o':
+                for d in range(4):
+                    cnt = 1
+                    nx = x + dx[d] * cnt
+                    ny = y + dy[d] * cnt
 
-            # o인 지점부터 델타탐색 시작
-            if omok[x][y] == 'o':
-                for z in range(4):
-                    nx = x
-                    ny = y
-                    cnt = 0
-
-                    # nx, ny가 범위 내인지 확인하고 o이라면 그 방향으로 쭉 탐색
-                    while 0 <= nx < n and 0 <= ny < n and omok[nx][ny] == 'o':
+                    while 0 <= nx < n and 0 <= ny < n and arr[nx][ny] == 'o':
                         cnt += 1
-                        nx += dx[z]
-                        ny += dy[z]
+                        nx = x + dx[d] * cnt
+                        ny = y + dy[d] * cnt
 
                     if cnt >= 5:
-                        return "YES"
+                        return 'YES'
 
-    return "NO"
+    return 'NO'
 
 for t in range(1, int(input()) + 1):
     n = int(input())
-    omok = [list(map(str, input())) for _ in range(n)]
-    print(f'#{t} {check(omok)}')
+    arr = [list(input()) for _ in range(n)]
+
+    print(f'#{t}', check(0, 0))
