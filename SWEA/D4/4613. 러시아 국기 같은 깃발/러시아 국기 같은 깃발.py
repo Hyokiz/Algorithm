675a7dabc9a8dc9ce05de9e981c7e58b9e95c969
HyeonHyuk Jang <109258146+Hyokiz@users.noreply.github.com>
2022-10-05 00:27:22 +0900

    [D4] Title: 러시아 국기 같은 깃발, Time: 590 ms, Memory: 62,888 KB -BaekjoonHub

--- SWEA/D4/4613. 러시아 국기 같은 깃발/러시아 국기 같은 깃발.py
+++ SWEA/D4/4613. 러시아 국기 같은 깃발/러시아 국기 같은 깃발.py
@@ -3,6 +3,7 @@
 for t in range(1, int(input()) + 1):
     n, m = map(int, input().split())
     arr = [list(input()) for _ in range(n)]
+    new_arr = []
     ans = []
     for i in range(0, n-2):
         for j in range(i+1, n-1):
@@ -21,3 +22,6 @@ for t in range(1, int(input()) + 1):
             ans.append(result_a + result_b + result_c)
 
     print(f'#{t}', min(ans))
+
+
+
