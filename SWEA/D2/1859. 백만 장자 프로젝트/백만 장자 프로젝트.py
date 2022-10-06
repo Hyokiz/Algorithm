c2c68f67290a1b580d967716ebcf9f958fed14e3
HyeonHyuk Jang <109258146+Hyokiz@users.noreply.github.com>
2022-10-06 11:36:29 +0900

    [D2] Title: 백만 장자 프로젝트, Time: 1,041 ms, Memory: 249,096 KB -BaekjoonHub

--- SWEA/D2/1859. 백만 장자 프로젝트/백만 장자 프로젝트.py
+++ SWEA/D2/1859. 백만 장자 프로젝트/백만 장자 프로젝트.py
@@ -2,8 +2,15 @@ for t in range(1, int(input()) + 1):
     n = int(input())
     prices = list(map(int, input().split()))
     result = 0
-
+    
+    # 가장 마지막 날 값을 max_value로 잡는다.
     max_value = prices[-1]
+
+    # 가장 마지막 날 값보다 전날 값이 더 크다면 갱신해준다.
+    # 이득은 result에 더한다.
+    # 마지막 날 가격 < 그 전날 가격 이라면 손해를 보기 때문
+    # 최대 이익을 보려면 가장 비쌀때 팔아야 한다.
+    
     for i in range(n-1, -1, -1):
         if prices[i] > max_value:
             max_value = prices[i]
