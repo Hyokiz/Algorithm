ceb27a224eb332939c2e28ae528757f94a630528
HyeonHyuk Jang <janghh97@gmail.com>
2022-10-12 17:15:15 +0900

    [Bronze I] Title: 최소공배수, Time: 152 ms, Memory: 32952 KB -BaekjoonHub

--- 백준/Bronze/1934. 최소공배수/최소공배수.py
+++ 백준/Bronze/1934. 최소공배수/최소공배수.py
@@ -1,22 +1,9 @@
-# 1934. 최소공배수
+import math
 
 t = int(input())
-for _ in range(t):
+
+for i in range(t):
     a, b = map(int, input().split())
-    ans = 0
-    if a > b:
-        for i in range(1, b + 1):
-            if (a * i) % b == 0:
-                ans = a * i
-                break
-
-    elif a < b:
-        for i in range(1, a + 1):
-            if (b * i) % a == 0:
-                ans = b * i
-                break
-
-    else:
-        ans = a
-
+    ans = math.lcm(a, b)
+    
     print(ans)
