47ee932f319e926c7467ae710f0ac24839df5b66
HyeonHyuk Jang <janghh97@gmail.com>
2022-10-12 17:15:48 +0900

    [Bronze I] Title: 최소공배수, Time: 72 ms, Memory: 32952 KB -BaekjoonHub

--- 백준/Bronze/1934. 최소공배수/최소공배수.py
+++ 백준/Bronze/1934. 최소공배수/최소공배수.py
@@ -1,9 +1,12 @@
+import sys
 import math
+
+input = sys.stdin.readline
 
 t = int(input())
 
 for i in range(t):
     a, b = map(int, input().split())
     ans = math.lcm(a, b)
-    
+
     print(ans)
