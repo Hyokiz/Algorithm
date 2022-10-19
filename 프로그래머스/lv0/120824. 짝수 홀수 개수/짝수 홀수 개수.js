53315ca5175a7bd9969a96dd3616a31672f13e6b
HyeonHyuk Jang <janghh97@gmail.com>
2022-10-20 08:53:27 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120824. 짝수 홀수 개수/짝수 홀수 개수.js
+++ 프로그래머스/lv0/120824. 짝수 홀수 개수/짝수 홀수 개수.js
@@ -1,15 +1,7 @@
 function solution(num_list) {
-    var answer = [];
-    let odd = 0;
-    let even = 0;
-    for (i of num_list) {
-        if (i % 2) {
-            odd ++
-        } else {
-            even ++
-        }
-    }
-    answer.push(even)
-    answer.push(odd)
+    var answer = [
+        num_list.filter((n) => n % 2 === 0).length,
+        num_list.filter((n) => n % 2 === 1).length,
+    ]
     return answer
 }
