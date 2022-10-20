78f3371ade2aeb86c3c4c980980a67d8084f8666
HyeonHyuk Jang <janghh97@gmail.com>
2022-10-21 00:15:22 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120895. 인덱스 바꾸기/인덱스 바꾸기.js
+++ 프로그래머스/lv0/120895. 인덱스 바꾸기/인덱스 바꾸기.js
@@ -1,6 +1,11 @@
 function solution(my_string, num1, num2) {
-    const result = [...my_string]
-    result.splice(num1, 1, my_string[num2])
-    result.splice(num2, 1, my_string[num1])
-    return result.join('')    
+    my_string = my_string.split('');
+    [my_string[num1], my_string[num2]] = [my_string[num2], my_string[num1]]
+    return my_string.join('')
 }
+
+// function solution(my_string, num1, num2) {
+//     my_string = my_string.split('');
+//     [my_string[num1], my_string[num2]] = [my_string[num2], my_string[num1]];
+//     return my_string.join('');
+// }
