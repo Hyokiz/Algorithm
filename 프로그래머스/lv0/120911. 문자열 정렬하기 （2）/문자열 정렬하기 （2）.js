de0cde29235406ad65c68431c2b530a46cac4e60
HyeonHyuk Jang <janghh97@gmail.com>
2022-10-21 00:20:19 +0900

    [level 0] Title: 문자열 정렬하기 (2), Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120911. 문자열 정렬하기 （2）/문자열 정렬하기 （2）.js
+++ 프로그래머스/lv0/120911. 문자열 정렬하기 （2）/문자열 정렬하기 （2）.js
@@ -1,7 +1,3 @@
 function solution(my_string) {
-    var answer = '';
-    my_string = my_string.toLowerCase();
-    const li = [...my_string]
-    li.sort()
-    return li.join('');
+    return my_string.toLowerCase().split('').sort().join('')
 }
