4981228d425820759dec6a1fe85aad610745ea59
HyeonHyuk Jang <janghh97@gmail.com>
2022-10-20 13:58:48 +0900

    [D3] Title: N-Queen, Time: 220 ms, Memory: 60,916 KB -BaekjoonHub

---
+++ SWEA/D3/2806. N－Queen/N－Queen.py
@@ -0,0 +1,25 @@
+for t in range(1, int(input()) + 1):
+    n = int(input())
+
+    ans = 0
+    col = [0] * 15
+
+    def check(x):
+        for i in range(x):
+            if col[x] == col[i] or abs(col[i] - col[x]) == x - i:
+                return False
+        return True
+
+    def dfs(x):
+        global ans
+        if x == n:
+            ans += 1
+            return
+
+        for i in range(n):
+            col[x] = i
+            if check(x):
+                dfs(x+1)
+
+    dfs(0)
+    print(f'#{t}', ans)
