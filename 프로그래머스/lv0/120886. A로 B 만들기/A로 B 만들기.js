5a9a6a571093388bae4bb8dec18a508d36309c95
HyeonHyuk Jang <janghh97@gmail.com>
2022-10-21 16:29:50 +0900

    [level 0] Title: A로 B 만들기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120886. A로 B 만들기/A로 B 만들기.js
+++ 프로그래머스/lv0/120886. A로 B 만들기/A로 B 만들기.js
@@ -1,4 +1,3 @@
 function solution(before, after) {
-  const sort = str => [...str].sort((a, b) => (a < b ? -1 : a !== b ? 1 : 0)).join('');
-  return sort(before) === sort(after) ? 1 : 0;
+    return before.split('').sort().join('') === after.split('').sort().join('') ? 1 : 0
 }
