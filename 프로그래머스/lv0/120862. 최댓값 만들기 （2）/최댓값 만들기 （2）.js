e92695076b6acd78ec780d1079768b49b3a686ac
HyeonHyuk Jang <janghh97@gmail.com>
2022-10-21 14:17:59 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 0.10 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.js
+++ 프로그래머스/lv0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.js
@@ -1,4 +1,4 @@
 function solution(numbers) {
     const newNumbers = numbers.sort((a, b) => { return a - b })
-    return newNumbers[0] * newNumbers[1] > newNumbers[newNumbers.length - 1]* newNumbers[newNumbers.length-2] ? newNumbers[0] * newNumbers[1] : newNumbers[newNumbers.length - 1]* newNumbers[newNumbers.length-2]
+    return Math.max(newNumbers[0] * newNumbers[1], newNumbers[newNumbers.length - 2] * newNumbers[newNumbers.length - 1])
 }
