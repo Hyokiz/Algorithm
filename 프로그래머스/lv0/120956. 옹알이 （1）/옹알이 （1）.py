7e78a19274bec1591e272b4f1ca3e5f97b8f7635
HyeonHyuk Jang <janghh97@gmail.com>
2023-03-21 17:34:52 +0900

    [level 0] Title: 옹알이 (1), Time: 0.04 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120956. 옹알이 （1）/옹알이 （1）.py
+++ 프로그래머스/lv0/120956. 옹알이 （1）/옹알이 （1）.py
@@ -1,14 +1,27 @@
-from itertools import permutations
+# from itertools import permutations
+# def solution(babbling):
+#     answer = 0
+#     speek = ["aya","ye","woo","ma"]
+#     word = set()
+#     for i in range(1, len(speek)+1):
+#         for j in permutations(speek, i):
+#             word.add(''.join(j))
+
+#     for i in babbling:
+#         if i in word:
+#             answer += 1
+
+#     return answer
+
 def solution(babbling):
     answer = 0
-    speek = ["aya","ye","woo","ma"]
-    word = set()
-    for i in range(1, len(speek)+1):
-        for j in permutations(speek, i):
-            word.add(''.join(j))
-
+    words = ["aya", "ye", "woo", "ma"]
     for i in babbling:
-        if i in word:
+        for j in words:
+            if not i * 2 in babbling:
+                i = i.replace(j, " ")
+                print(i)
+        if len(i.strip()) == 0:
             answer += 1
-
+    
     return answer
