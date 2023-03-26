02ba6011a6e7ecd63f8e9181ed3961ebc2a5234e
HyeonHyuk Jang <janghh97@gmail.com>
2023-03-26 15:45:27 +0900

    [level 0] Title: k의 개수, Time: 8.61 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120887. k의 개수/k의 개수.py
+++ 프로그래머스/lv0/120887. k의 개수/k의 개수.py
@@ -1,8 +1,5 @@
 def solution(i, j, k):
     answer = 0
-    li = list(map(str, range(i, j + 1)))
-    for s in li:
-        for u in s:
-            if str(k) == u:
-                answer += 1
+    for s in range(i, j + 1):
+        answer += str(s).count(str(k))
     return answer
