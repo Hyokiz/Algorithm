9956dc4818558d6e7aeec8e4d14b4d33256cbfd4
HyeonHyuk Jang <janghh97@gmail.com>
2023-04-01 19:06:00 +0900

    [unrated] Title: 추억 점수, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/176963. 추억 점수/추억 점수.py
@@ -0,0 +1,14 @@
+def solution(name, yearning, photo):
+    answer = []
+    score = dict()
+    for i in range(len(name)):
+        score[name[i]] = yearning[i]
+
+    
+    for i in photo:
+        cnt = 0
+        for j in i:
+            if j in score:
+                cnt += score[j]
+        answer.append(cnt)
+    return answer
