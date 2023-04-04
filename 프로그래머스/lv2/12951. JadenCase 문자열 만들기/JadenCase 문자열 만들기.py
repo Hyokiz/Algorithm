dc3ff986e483c8d0a12875a9fe6ea8c546ac463f
HyeonHyuk Jang <janghh97@gmail.com>
2023-04-04 16:46:40 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.00 ms, Memory: 10 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.py
@@ -0,0 +1,15 @@
+# def solution(s):
+#     answer = ""
+#     words = s.split(" ")
+#     print(ord("0")) # 48
+#     print(ord("9")) # 57
+#     for i in range(len(words)):
+#         if 48 <= ord(words[i][0]) <= 57:
+#             continue
+#         else:
+#             words[i] = words[i].capitalize()
+#     answer = " ".join(words)
+    
+#     return answer
+def solution(s):
+    return " ".join([word.capitalize() for word in s.split(" ")])
