e84314c3d13233834363fa421c51554c2214ea5b
HyeonHyuk Jang <janghh97@gmail.com>
2023-08-28 20:05:04 +0900

    [unrated] Title: 콜라 문제, Time: 0.00 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/unrated/132267. 콜라 문제/콜라 문제.py
+++ 프로그래머스/unrated/132267. 콜라 문제/콜라 문제.py
@@ -1,9 +1,7 @@
 def solution(a, b, n):
     answer = 0
     while n >= a:
-        r = n % a
-        n = (n // a) * b
-        answer += n
-        n += r
+        answer += (n // a) * b
+        n = (n // a) * b + (n % a)
         
     return answer
