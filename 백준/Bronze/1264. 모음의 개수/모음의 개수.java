ff55757d086d3e00adda07bff9f8a7e392f094b2
HyeonHyuk Jang <janghh97@gmail.com>
2023-09-21 16:31:46 +0900

    [Bronze IV] Title: 모음의 개수, Time: 220 ms, Memory: 17616 KB -BaekjoonHub

---
+++ 백준/Bronze/1264. 모음의 개수/모음의 개수.java
@@ -0,0 +1,30 @@
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        char[] vowels = {'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'};
+
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            int answer = 0;
+            String s = sc.nextLine();
+            if (s.equals("#")) {
+                break;
+            }
+
+            for (char x: s.toCharArray()) {
+                for (int i=0; i<10; i++) {
+                    if (x == vowels[i]) {
+                        answer++;
+                    }
+                }
+            }
+            System.out.println(answer);
+        }
+
+    }
+
+}
+
