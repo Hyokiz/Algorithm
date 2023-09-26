b7afdbfee409e8d5d8bafc1258e2f8aaaad089e6
HyeonHyuk Jang <janghh97@gmail.com>
2023-09-26 22:40:35 +0900

    [D3] Title: 문자열문자열, Time: 141 ms, Memory: 58,504 KB -BaekjoonHub

--- SWEA/D3/17319. 문자열문자열/문자열문자열.py
+++ SWEA/D3/17319. 문자열문자열/문자열문자열.py
@@ -3,9 +3,5 @@
 for t in range(1, int(input()) + 1):
     n = int(input())
     s = input()
-    answer = "No"
-    if n % 2 == 0:
-        if s[:n//2] == s[n//2:]:
-            answer = "Yes"
-
+    answer = "Yes" if n % 2 == 0 and s[:n//2] == s[n//2:] else "No"
     print(f'#{t} {answer}')
