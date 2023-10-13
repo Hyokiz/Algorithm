4d458458f5eff02bd813280cb147951d235af139
HyeonHyuk Jang <janghh97@gmail.com>
2023-10-13 13:38:34 +0900

    [level unrated] Title: 세로 읽기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181904. 세로 읽기/세로 읽기.py
+++ 프로그래머스/unrated/181904. 세로 읽기/세로 읽기.py
@@ -1,4 +1,2 @@
 def solution(my_string, m, c):
-    strings = [my_string[i:i+m] for i in range(0, len(my_string), m)]
-    temp = [i[c-1] for i in strings]
-    return "".join(temp)
+    return my_string[c-1::m]
