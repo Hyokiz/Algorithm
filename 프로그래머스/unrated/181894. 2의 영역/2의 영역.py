79c53a4996217a8913ab8f9745299b2499a00794
HyeonHyuk Jang <janghh97@gmail.com>
2023-10-27 14:47:24 +0900

    [level unrated] Title: 2의 영역, Time: 0.54 ms, Memory: 15.8 MB -BaekjoonHub

--- 프로그래머스/unrated/181894. 2의 영역/2의 영역.py
+++ 프로그래머스/unrated/181894. 2의 영역/2의 영역.py
@@ -1,15 +1,5 @@
 def solution(arr):
-    answer = []
-    
-    if 2 in arr:
-        if arr.count(2) > 1:
-            start = arr.index(2)
-            end = len(arr) - arr[::-1].index(2)
-            return arr[start:end]
-        else:
-            idx = arr.index(2)
-            return [arr[idx]]
-    else:
+    if 2 not in arr:
         return [-1]
-    
-    return answer
+    else:
+        return arr[arr.index(2) : len(arr) - arr[::-1].index(2)]
