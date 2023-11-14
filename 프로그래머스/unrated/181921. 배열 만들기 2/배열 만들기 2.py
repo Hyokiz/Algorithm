af8845ef0697a81e4fb72c93fca3436bc96ca56b
HyeonHyuk Jang <janghh97@gmail.com>
2023-11-14 14:11:34 +0900

    [level unrated] Title: 배열 만들기 2, Time: 29.77 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/unrated/181921. 배열 만들기 2/배열 만들기 2.py
+++ 프로그래머스/unrated/181921. 배열 만들기 2/배열 만들기 2.py
@@ -1,8 +1,6 @@
 def solution(l, r):
     answer = []
     for i in range(l, r + 1):
-        if i % 5 == 0:
-            
-            if not str(i).replace("5", "").replace("0", ""):
-                answer.append(i)
+        if not set(str(i)) - set(["0", "5"]):
+            answer.append(i)
     return answer if answer else [-1]
